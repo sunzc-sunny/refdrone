f220a1751d633f5d3999cba113592758eb88eb9d
sunzhichao <72689555+sunzc-sunny@users.noreply.github.com>
2025-05-20 17:28:51 +0800

    Update __init__.py

--- mmdet/models/data_preprocessors/__init__.py
+++ mmdet/models/data_preprocessors/__init__.py
@@ -2,14 +2,12 @@
 from .data_preprocessor import (BatchFixedSizePad, BatchResize,
                                 BatchSyncRandomResize, BoxInstDataPreprocessor,
                                 DetDataPreprocessor,
-                                MultiBranchDataPreprocessor,
-                                LLavaDetDataPreprocessor,
-                                LLavaDetCountDataPreprocessor)
+                                MultiBranchDataPreprocessor)
 from .reid_data_preprocessor import ReIDDataPreprocessor
 from .track_data_preprocessor import TrackDataPreprocessor
 
 __all__ = [
     'DetDataPreprocessor', 'BatchSyncRandomResize', 'BatchFixedSizePad',
     'MultiBranchDataPreprocessor', 'BatchResize', 'BoxInstDataPreprocessor',
-    'TrackDataPreprocessor', 'ReIDDataPreprocessor', 'LLavaDetDataPreprocessor', 'LLavaDetCountDataPreprocessor'
+    'TrackDataPreprocessor', 'ReIDDataPreprocessor'
 ]
